155a2805b0ab0551b2b2b947e27fa48b766e9cf5
Student <Student@193.107.34.215>
2018-03-06 13:44:58 +0100

    Struktura projektu

--- Shop2018/src/shop2018/koszyk.java
+++ Shop2018/src/shop2018/koszyk.java
@@ -2,31 +2,40 @@
 package shop2018;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+import java.util.HashMap;
+//import java.util.List;
+import java.util.Map;
 
 
 public class koszyk {
-    private List<produkt> lista = new ArrayList();
+    //private List<produkt> lista = new ArrayList();
+    
+    private Map<produkt, Integer> map = new HashMap();
     
     public void dodaj(produkt p)
     {
-        lista.add(p);
+       // lista.add(p);
+        map.put(p,1);
     }
     
     public BigDecimal doZaplaty()
     {
         BigDecimal suma = BigDecimal.ZERO;
-        for (produkt p : lista)
+        //for (produkt p : lista)
+        for(produkt p : map.keySet())
         {
-           suma=suma.add(p.gCena());
+            int ilosc = map.get(p);
+           //suma=suma.add(p.gCena());
+           suma = suma.add(p.gCena().multiply(new BigDecimal(ilosc)));
         }
         return suma;
     }
     
     public String toString()
     {
-        return lista.toString()+" "+doZaplaty();
+        //return lista.toString()+" "+doZaplaty();
+        return map.toString()+" "+doZaplaty();
     }
     
 }
