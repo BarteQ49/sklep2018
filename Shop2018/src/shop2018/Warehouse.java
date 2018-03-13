a03e23da471c5b63cd1d979e356e1adc6a0c516b
Student <Student@193.107.34.215>
2018-03-13 13:15:47 +0100

    magazyn

---
+++ Shop2018/src/shop2018/Warehouse.java
@@ -0,0 +1,51 @@
+
+package shop2018;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.TreeSet;
+
+
+public class Warehouse {
+    private List<product> products = new ArrayList();
+    
+    public Warehouse()
+    {
+        products.add(new product("Chleb", new BigDecimal("2.1")));
+        products.add(new product("Olej", new BigDecimal("3.1")));
+        products.add(new product("Woda", new BigDecimal("1.8")));
+        products.add(new product("Bulka", new BigDecimal("0.45")));
+        
+    }
+    
+    public Collection<product> productsSortByName()
+    {
+        TreeSet set = new TreeSet();
+        set.addAll(products);
+        return set;
+    }
+    
+    
+    
+    
+    public Collection<product> productsSortByPrice()
+    {
+        return null;
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        return products.toString();
+    }
+    
+    public static void main(String[] args) {
+        Warehouse w = new Warehouse();
+        System.out.println(w);
+        System.out.println(w.productsSortByName());
+    }
+    
+}
